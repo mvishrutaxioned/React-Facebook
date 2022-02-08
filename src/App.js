ef12fa45412885d4a087d0d321b66147a68b1e5a
VishrutMargam <vishrutm@axioned.com>
2022-02-08 12:41:16 +0530

    added AddPost

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
+import AddPost from './components/AddPost';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddPost />
     </div>
   );
 }
---
+++ src/components/AddPost.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const AddPost = () => {
+  return (
+    <section class="addPost">
+        <div class="wrapper">
+        <div class="send">
+            <span class="icon user">user</span>
+            <form action="#FIXME" method="GET" id="searchForm">
+            <div> <input type="text" placeholder="What's on your mind, Vishrut ?" id="addInput" /></div>
+            </form>
+        </div>
+        <ul>
+            <li><a href="#FIXME" title="Live Video">Live video</a></li>
+            <li><a href="#FIXME" title="Photo / Video">Photo/video</a></li>
+            <li><a href="#FIXME" title="Feeling / Activity">Feeling/Activity</a></li>
+        </ul>
+        </div>
+    </section>
+  );
+};
+
+export default AddPost;
